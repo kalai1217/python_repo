e0637b3c7105f661f8219fa9315c55677b249d36
kalai1217 <kalaiarasan.j@diggibyte.com>
2024-03-13 10:58:48 +0530

    Runner-Up problem of O(2n) complexity

---
+++ src/runnerup_score/driver.py
@@ -0,0 +1,2 @@
+from utils import runnerup_score
+runnerup_score()
---
+++ src/runnerup_score/utils.py
@@ -0,0 +1,17 @@
+import logging
+def runnerup_score():
+    logging.basicConfig(level=logging.INFO,format="%(message)s")
+    n = int(input())
+    arr = list(map(int, input().split()))
+    sorted_array = sorted(arr)
+    n = len(sorted_array)
+    largest = sorted_array[0]
+    for i in range(n):
+        if (sorted_array[i] > largest):
+            largest = sorted_array[i]
+    second_largest = 0
+    for i in range(n - 2, -1, -1):
+        if (sorted_array[i] != largest):
+            second_largest = sorted_array[i]
+            break
+    logging.info(second_largest)
