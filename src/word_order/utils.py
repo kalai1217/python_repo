61d2de9596f0efb5ff7b50fdbe08ef2fd8bbc760
kalai1217 <kalaiarasan.j@diggibyte.com>
2024-03-18 00:20:31 +0530

    count the number of occurrences of each word

--- src/word_order/utils.py
+++ src/word_order/utils.py
@@ -1,15 +1,15 @@
+from collections import Counter
 import logging
-logging.basicConfig(level=logging.INFO,format="%(message)s")
+
+logging.basicConfig(level=logging.INFO, format="%(message)s")
 def word_order():
-    n_ish = int(input())
-    counter_dict = {}
-    words_list = []
-    for i in range(n_ish):
-        word = input()
-        words_list.append(word)
-        if word in counter_dict:
-            counter_dict[word] += 1
-        else:
-            counter_dict[word] = 1
-    logging.info(len(counter_dict))
-    logging.info(' '.join([str(counter_dict[word]) for word in counter_dict]))
+    ans = []
+    n = int(input())
+    ls = []
+    for _ in range(n):
+        ls.append(input())
+    dic = Counter(ls)
+    ans.append(str(len(dic.keys())))
+    ans.append(list(dic.values()))
+    logging.info(ans)
+    return str(ans)
