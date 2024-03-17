674ab5855974b45622b54e932b285130d9867658
kalai1217 <kalaiarasan.j@diggibyte.com>
2024-03-18 00:13:47 +0530

    Unit test for word_order

---
+++ test/word_order/test_word_order.py
@@ -0,0 +1,11 @@
+import unittest
+from src.word_order.utils import *
+class test_no_idea(unittest.TestCase):
+    def test_na1(self):
+        def test_one(self):
+            self.assertEqual(wordOrder(), "['3', [2, 1, 1]]")
+            '''4
+    bcdef
+    abcdefg
+    bcde
+    bcdef'''
