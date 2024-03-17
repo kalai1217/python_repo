c23116bfa726d933d4ca73076397471e0130e2db
kalai1217 <kalaiarasan.j@diggibyte.com>
2024-03-18 00:21:46 +0530

    Text alignment using rjust,ljust and center

--- src/text_alignment/utils.py
+++ src/text_alignment/utils.py
@@ -1,24 +1,34 @@
+import logging
+logging.basicConfig(level= logging.INFO, format= '%(message)s')
 def text_alignment():
-    thickness = int(input())  # This must be an odd number
+    thickness = int(input())
     c = 'H'
-
-    # Top Cone
+    ans = ''
+    # Top cone
     for i in range(thickness):
-        print((c * i).rjust(thickness - 1) + c + (c * i).ljust(thickness - 1))
-
-    # Top Pillars
+        logging.info(((c * i).rjust(thickness - 1) + c + (c * i).ljust(thickness - 1)))
+        temp = ((c * i).rjust(thickness - 1) + c + (c * i).ljust(thickness - 1))
+        ans += temp + '\n'
+    # Top pillars
     for i in range(thickness + 1):
-        print((c * thickness).center(thickness * 2) + (c * thickness).center(thickness * 6))
-
-    # Middle Belt
+        logging.info(((c * thickness).center(thickness * 2) + (c * thickness).center(thickness * 6)))
+        temp = ((c * thickness).center(thickness * 2) + (c * thickness).center(thickness * 6))
+        ans += temp + '\n'
+    # Middle belt
     for i in range((thickness + 1) // 2):
-        print((c * thickness * 5).center(thickness * 6))
-
-    # Bottom Pillars
+        logging.info(((c * thickness * 5).center(thickness * 6)))
+        temp = ((c * thickness * 5).center(thickness * 6))
+        ans += temp + '\n'
+    # Bottom pillars
     for i in range(thickness + 1):
-        print((c * thickness).center(thickness * 2) + (c * thickness).center(thickness * 6))
-
-    # Bottom Cone
+        logging.info(((c * thickness).center(thickness * 2) + (c * thickness).center(thickness * 6)))
+        temp = ((c * thickness).center(thickness * 2) + (c * thickness).center(thickness * 6))
+        ans += temp + '\n'
+    # Bottom cone
     for i in range(thickness):
-        print(((c * (thickness - i - 1)).rjust(thickness) + c + (c * (thickness - i - 1)).ljust(thickness)).rjust(
+        logging.info((((c * (thickness - i - 1)).rjust(thickness) + c + (c * (thickness - i - 1)).ljust(
+            thickness)).rjust(thickness * 6)))
+        temp = (((c * (thickness - i - 1)).rjust(thickness) + c + (c * (thickness - i - 1)).ljust(thickness)).rjust(
             thickness * 6))
+        ans += temp + '\n'
+    return ans
